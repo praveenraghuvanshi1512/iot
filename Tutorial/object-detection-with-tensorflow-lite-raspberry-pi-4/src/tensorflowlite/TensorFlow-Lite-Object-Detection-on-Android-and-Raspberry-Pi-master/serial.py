b29bcf1768de6fadfb5978641497d1d492761c11
Praveen Raghuvanshi <praveen_iiitmk@yahoo.com>
2020-02-02 01:47:55 +0530

    Tutorial: Removed unnecessary methods

--- Tutorial/object-detection-with-tensorflow-lite-raspberry-pi-4/src/tensorflowlite/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi-master/serial.py
+++ Tutorial/object-detection-with-tensorflow-lite-raspberry-pi-4/src/tensorflowlite/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi-master/serial.py
@@ -18,32 +18,3 @@ class SerialEx:
         print('Data : ' + data)
         self.ser.write(str.encode(data))
         time.sleep(1)
-    
-    def blinkLed(self):
-        print('**** GPIOEX::Person Detected')
-        if(self.ser.isOpen()):
-            print('LED On')
-            self.ser.write(str.encode('LED ON'))
-            self.turnOnATCommands()
-            time.sleep(2)
-            print('LED Off')
-            self.turnOffATCommands()
-        
-    def turnOnATCommands(self):
-        print('Turn on')
-        self.ser.write(str.encode('+++'))
-        time.sleep(0.5)
-        print(self.ser.read(100))
-        time.sleep(2)
-        self.ser.write(str.encode('ATD05\r\n'))
-        time.sleep(0.5)
-        print(self.ser.read(100))
-        self.ser.write(str.encode('ATDCN\r\n'))
-        time.sleep(0.5)
-        print(self.ser.read(100))
-                
-    def turnOffATCommands(self):
-        self.ser.write(str.encode('+++'))
-        time.sleep(2)
-        self.ser.write(str.encode('ATD04\r\n'))
-        self.ser.write(str.encode('ATDCN\r\n'))
